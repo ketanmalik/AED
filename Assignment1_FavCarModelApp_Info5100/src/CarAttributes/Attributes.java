4e172ae20a836d04509b05a12677c2078b072a27
ketanmalik <malik.ke@husky.neu.edu>
2019-09-21 16:53:38 -0400

    Assignment1

---
+++ Assignment1_FavCarModelApp_Info5100/src/CarAttributes/Attributes.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CarAttributes;
+
+/**
+ *
+ * @author ketanmalik
+ */
+public class Attributes {
+    private String make;
+    private String modelNo;
+    private String vehicleType;
+    private String releaseYear;
+    private String color;
+    private String seatingCapacity;
+    private String range;
+    private String cargoRoom;
+    private String chargeTime;
+    private String autoPilotFeature;
+    private String fourWheelDrive;
+    private String displayPanelSize;
+    private String speakerNo;
+    private String warranty;
+    private String weight;
+    private String carImage;
+    private String displayImage;
+
+    public String getDisplayImage() {
+        return displayImage;
+    }
+
+    public void setDisplayImage(String displayImage) {
+        this.displayImage = displayImage;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModelNo() {
+        return modelNo;
+    }
+
+    public void setModelNo(String modelNo) {
+        this.modelNo = modelNo;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSeatingCapacity() {
+        return seatingCapacity;
+    }
+
+    public void setSeatingCapacity(String seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getCargoRoom() {
+        return cargoRoom;
+    }
+
+    public void setCargoRoom(String cargoRoom) {
+        this.cargoRoom = cargoRoom;
+    }
+
+    public String getChargeTime() {
+        return chargeTime;
+    }
+
+    public void setChargeTime(String chargeTime) {
+        this.chargeTime = chargeTime;
+    }
+
+    public String getAutoPilotFeature() {
+        return autoPilotFeature;
+    }
+
+    public void setAutoPilotFeature(String autoPilotFeature) {
+        this.autoPilotFeature = autoPilotFeature;
+    }
+
+    public String getFourWheelDrive() {
+        return fourWheelDrive;
+    }
+
+    public void setFourWheelDrive(String fourWheelDrive) {
+        this.fourWheelDrive = fourWheelDrive;
+    }
+
+    public String getDisplayPanelSize() {
+        return displayPanelSize;
+    }
+
+    public void setDisplayPanelSize(String displayPanelSize) {
+        this.displayPanelSize = displayPanelSize;
+    }
+
+    public String getSpeakerNo() {
+        return speakerNo;
+    }
+
+    public void setSpeakerNo(String speakerNo) {
+        this.speakerNo = speakerNo;
+    }
+
+    public String getWarranty() {
+        return warranty;
+    }
+
+    public void setWarranty(String warranty) {
+        this.warranty = warranty;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getCarImage() {
+        return carImage;
+    }
+
+    public void setCarImage(String carImage) {
+        this.carImage = carImage;
+    }
+   
+    
+}
