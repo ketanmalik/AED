29b7387b99bec3310fb7beab67eb9ce7b73ccc58
ketanmalik <malik.ke@husky.neu.edu>
2019-09-29 22:19:05 -0400

    Assignment 3 Add Car Functionality

---
+++ Assignment3_UberApp/src/Business/CarAttributes.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ketanmalik
+ */
+public class CarAttributes {
+
+    private String name;
+    private int modelNo;
+    private int serialNo;
+    private String manufacturer;
+    private int yearOfManufacture;
+    private int capacity;
+    private boolean availability;
+    private String color;
+    private Date dateOfCreation;
+    private boolean maintenanceCertificate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getModelNo() {
+        return modelNo;
+    }
+
+    public void setModelNo(int modelNo) {
+        this.modelNo = modelNo;
+    }
+
+    public int getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(int serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Date getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public void setDateOfCreation(Date dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+
+    public boolean isMaintenanceCertificate() {
+        return maintenanceCertificate;
+    }
+
+    public void setMaintenanceCertificate(boolean maintenanceCertificate) {
+        this.maintenanceCertificate = maintenanceCertificate;
+    }
+
+}
---
+++ Assignment3_UberApp/src/Business/CarFleet.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ketanmalik
+ */
+public class CarFleet {
+
+    private ArrayList<CarAttributes> carFleet;
+
+    public CarFleet() {
+        carFleet = new ArrayList<CarAttributes>();
+    }
+
+    public ArrayList<CarAttributes> getCarFleet() {
+        return carFleet;
+    }
+
+    public void setCarFleet(ArrayList<CarAttributes> carFleet) {
+        this.carFleet = carFleet;
+    }
+    
+    public CarAttributes addCar(){
+        CarAttributes carObject = new CarAttributes();
+        carFleet.add(carObject);
+        return carObject;
+    }
+
+}
