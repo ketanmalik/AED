4d2d9585fe4388de71d0bc99b13702cb802584a6
ketanmalik <malik.ke@husky.neu.edu>
2019-10-18 14:39:24 -0400

    Adding Lab Org Directory

--- Lab_5_skeleton/src/Business/ConfigureABusiness.java
+++ Lab_5_skeleton/src/Business/ConfigureABusiness.java
@@ -6,6 +6,7 @@ package Business;
 
 import Business.Employee.Employee;
 import Business.Organization.AdminOrganization;
+import Business.Organization.LabOrganization;
 import Business.UserAccount.UserAccount;
 
 /**
@@ -13,32 +14,42 @@ import Business.UserAccount.UserAccount;
  * @author ran
  */
 public class ConfigureABusiness {
-    
-    public static Business configure(){
+
+    public static Business configure() {
         // Three roles: LabAssistant, Doctor, Admin
-        
+
         Business business = Business.getInstance();
-        
+
         AdminOrganization adminOrganization = new AdminOrganization();
-        
+        LabOrganization labOrganization = new LabOrganization();
+
         business.getOrganizationDirectory().getOrganizationList().add(adminOrganization);
-        
+        business.getOrganizationDirectory().getOrganizationList().add(labOrganization);
+
         Employee employee = new Employee();
         employee.setName("Abc Xyz");
-        
+        Employee employee1 = new Employee();
+        employee1.setName("Ben");
+
         UserAccount account = new UserAccount();
         account.setUsername("admin");
         account.setPassword("admin");
         account.setRole("Admin");
-        
+        UserAccount account1 = new UserAccount();
+        account1.setUsername("lab");
+        account1.setPassword("lab");
+        account1.setRole("Lab");
+
         account.setEmployee(employee);
-        
+        account1.setEmployee(employee1);
+
         adminOrganization.getEmployeeDirectory().getEmployeeList().add(employee);
-        
+        labOrganization.getEmployeeDirectory().getEmployeeList().add(employee1);
+
         adminOrganization.getUserAccountDirectory().getUserAccountList().add(account);
-        
-        
+        labOrganization.getUserAccountDirectory().getUserAccountList().add(account1);
+
         return business;
     }
-    
+
 }
